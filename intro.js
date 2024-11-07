954d2ed8d221185b3e879a1202a6507e105f19a4
mollywong111 <mollywong1989@gmail.com>
2024-11-06 17:23:19 -0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,49 @@
+sheep.addEventListener("click", function(){
+
+    document.getElementById("cover").src="sheep.webp";
+    document.getElementById("sheep").style.backgroundColor="#97bf04";
+    document.getElementById("bear").style.backgroundColor="#d9043d";
+    document.getElementById("moose").style.backgroundColor="#d9043d";
+    document.getElementById("wolf").style.backgroundColor="#d9043d";
+    document.getElementById("elk").style.backgroundColor="#d9043d";
+});
+
+bear.addEventListener("click", function(){
+
+    document.getElementById("cover").src="bear.webp";
+    document.getElementById("bear").style.backgroundColor="#97bf04";
+    document.getElementById("sheep").style.backgroundColor="#d9043d";
+    document.getElementById("moose").style.backgroundColor="#d9043d";
+    document.getElementById("wolf").style.backgroundColor="#d9043d";
+    document.getElementById("elk").style.backgroundColor="#d9043d";
+});
+
+moose.addEventListener("click", function(){
+
+    document.getElementById("cover").src="moose.webp";
+    document.getElementById("moose").style.backgroundColor="#97bf04";
+    document.getElementById("bear").style.backgroundColor="#d9043d";
+    document.getElementById("sheep").style.backgroundColor="#d9043d";
+    document.getElementById("wolf").style.backgroundColor="#d9043d";
+    document.getElementById("elk").style.backgroundColor="#d9043d";
+});
+
+wolf.addEventListener("click", function(){
+
+    document.getElementById("cover").src="wolf.webp";
+    document.getElementById("wolf").style.backgroundColor="#97bf04";
+    document.getElementById("bear").style.backgroundColor="#d9043d";
+    document.getElementById("moose").style.backgroundColor="#d9043d";
+    document.getElementById("sheep").style.backgroundColor="#d9043d";
+    document.getElementById("elk").style.backgroundColor="#d9043d";
+});
+
+elk.addEventListener("click", function(){
+
+    document.getElementById("cover").src="elk.webp";
+    document.getElementById("elk").style.backgroundColor="#97bf04";
+    document.getElementById("bear").style.backgroundColor="#d9043d";
+    document.getElementById("moose").style.backgroundColor="#d9043d";
+    document.getElementById("wolf").style.backgroundColor="#d9043d";
+    document.getElementById("sheep").style.backgroundColor="#d9043d";
+});
